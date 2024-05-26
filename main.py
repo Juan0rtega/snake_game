24a652c735e33b22d618759b13b8d20b09181d5c
Juan Ortega <juan.ortega@theksquaregroup.com>
2024-05-26 08:27:28 -0600

    initial project data

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import pygame
+
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode ((600,600))
+    pygame.display.set_caption('snake game')
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        pygame.display.flip()
+    
+    pygame.quit()
+    
+if __name__ == '__main__': main()
